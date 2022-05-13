dd20d8974736a1b2672304800b3c88eadc6e9eb3
гений <603850549@qq.com>
2022-05-13 13:45:52 +0800

    新增代码

---
+++ src/components/request-btn/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-button :="attrs" @click="send" :loading="loading"></el-button>
+</template>
+
+<script setup lang="ts">
+import { useAttrs, ref, Ref, useSlots } from "vue";
+
+const attrs = useAttrs();
+const slots = useSlots();
+
+console.log(slots);
+
+const loading: Ref<boolean> = ref<boolean>(false);
+
+const { request } = defineProps<{
+  request: (loading: Ref<boolean>) => Promise<string>;
+}>();
+
+const send = () => {
+  request(loading);
+};
+</script>
+
+<style lang="less" scoped>
+.wrap {
+}
+</style>
